174310d892ea001523ba218274cff4ef919823b6
vgrigoryevich <v.grigoryevich@gmail.com>
2019-11-21 18:12:50 +0200

    task05, changed change to input. now work

--- script/task-05.js
+++ script/task-05.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('input#name-input');
 const output = document.querySelector('span#name-output');
 
-input.addEventListener('change', elem => {
+input.addEventListener('input', elem => {
     output.textContent = elem.currentTarget.value;
     if (elem.currentTarget.value.length === 0) {
         output.textContent = 'незнакомец';
